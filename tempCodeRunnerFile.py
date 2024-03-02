e0b7ab24ac6854c709a2fda392691764457ade4d
Anay <anaypund123@gmail.com>
2024-03-02 19:47:03 +0530

    Commited

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+devices = AudioUtilities.GetSpeakers()
+# interface = devices.Activate(
+#     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+# volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volRange = volume.GetVolumeRange()   #(-63.5, 0.0, 0.5) min max
